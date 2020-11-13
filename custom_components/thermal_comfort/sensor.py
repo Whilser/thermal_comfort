387c8fb349acbb2d2a6aa2ba40e7c053ba3cec8b
Whilser <34806483+Whilser@users.noreply.github.com>
2020-11-13 11:40:05 +0500

    Update sensor.py

--- custom_components/thermal_comfort/sensor.py
+++ custom_components/thermal_comfort/sensor.py
@@ -83,7 +83,7 @@ class SensorThermalComfort(Entity):
         """Initialize the sensor."""
         self.hass = hass
         self.entity_id = async_generate_entity_id(ENTITY_ID_FORMAT, "{}_{}".format(device_id, sensor_type), hass=hass)
-        self._name = "{} {}".format(friendly_name, SENSOR_TYPES[sensor_type][1])
+        self._name = "{}".format(SENSOR_TYPES[sensor_type][1])
         self._unit_of_measurement = SENSOR_TYPES[sensor_type][2]
         self._state = None
         self._device_state_attributes = {}
